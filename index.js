43cb4cff30d71816faf89b00aaf5d18265e2d7fc
geekplux <geekplux@qq.com>
2014-05-05 22:57:20 +0800

    Implement The Mini-Harp Module

---
+++ index.js
@@ -0,0 +1,9 @@
+var createMiniHarp = function() {
+  var connect = require('connect'),
+      app = connect();
+
+  // console.log("Starting mini-harp on http://localhost:4000");
+  // app.listen(4000);
+}
+
+module.exports = createMiniHarp;
